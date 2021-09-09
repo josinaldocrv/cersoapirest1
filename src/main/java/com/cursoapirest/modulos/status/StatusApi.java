028498eac9298a1737acc2587eeb9f7e36ea6c0e
josinaldocrv <84753670+josinaldocrv@users.noreply.github.com>
2021-09-09 03:08:25 -0300

    Dev - primeiro commit

---
+++ src/main/java/com/cursoapirest/modulos/status/StatusApi.java
@@ -0,0 +1,14 @@
+package com.cursoapirest.modulos.status;
+
+import org.springframework.http.ResponseEntity;
+
+public interface StatusApi {
+
+    @APIResponses({
+            @APIResponse(responseCode = "200", description = "Ok Api funcionando"),
+            @APIResponse(responseCode = "404", description = "Não encontrou a Api"),
+            @APIResponse(responseCode = "401", description = "Não tem permissão"),
+    })
+    ResponseEntity<StatusDTO> getStatus();
+
+}
---
+++ src/main/java/com/cursoapirest/modulos/status/StatusRest.java
@@ -0,0 +1,21 @@
+package com.cursoapirest.modulos.status;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusRest implements StatusApi {
+
+    @APIResponses({
+            @APIResponse(responseCode = "200", description = "Ok Api funcionando"),
+            @APIResponse(responseCode = "404", description = "Não encontrou a Api"),
+            @APIResponse(responseCode = "401", description = "Não tem permissão"),
+    })
+    @GetMapping(value = "status")
+    @Override
+    public ResponseEntity<StatusDTO> getStatus() {
+        StatusDTO statusDTO = new StatusDTO();
+        return ResponseEntity.ok().body(statusDTO);
+    }
+}
