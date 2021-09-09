91635607e2235814ce9ee004c2a4f1507c052e11
josinaldocrv <84753670+josinaldocrv@users.noreply.github.com>
2021-09-09 19:57:59 -0300

    Dev - add pessoa fisica repositorio.

--- src/main/java/com/cursoapirest/modulos/status/rest/APIResponse.java
+++ src/main/java/com/cursoapirest/modulos/status/rest/APIResponse.java
@@ -1,4 +1,7 @@
 package com.cursoapirest.modulos.status.rest;
 
 public @interface APIResponse {
+    String responseCode();
+
+    String description();
 }
--- src/main/java/com/cursoapirest/modulos/status/rest/StatusApi.java
+++ src/main/java/com/cursoapirest/modulos/status/rest/StatusApi.java
@@ -1,15 +1,5 @@
 package com.cursoapirest.modulos.status.rest;
 
-import com.cursoapirest.modulos.status.dto.StatusDTO;
-import org.springframework.http.ResponseEntity;
-
 public interface StatusApi {
-
-    @APIResponses({
-            @APIResponse(responseCode = "200", description = "Ok Api funcionando"),
-            @APIResponse(responseCode = "404", description = "Não encontrou a Api"),
-            @APIResponse(responseCode = "401", description = "Não tem permissão"),
-    })
-    ResponseEntity<StatusDTO> getStatus();
 
 }
